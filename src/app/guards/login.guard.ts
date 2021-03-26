802bad09e1cb713bba4d4b41d262af806d61b817
OzyyDemir <oguzhandmr98@gmail.com>
2021-03-26 21:05:07 +0300

    commtt

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -21,6 +21,8 @@ export class LoginGuard implements CanActivate {
         return true;
       }
       else{
+
+        
         this.router.navigate(["login"])
         this.toastrService.info("Sisteme giriş yapmalısınız.");
         return false;
